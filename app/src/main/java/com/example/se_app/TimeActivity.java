70c7fc156af7bd79a15788bd57d26907ef928fc0
최서연 <chltjdus1@gmail.com>
2023-11-20 10:43:35 +0900

    dto 수정

--- app/src/main/java/com/example/se_app/TimeActivity.java
+++ app/src/main/java/com/example/se_app/TimeActivity.java
@@ -1,11 +1,13 @@
 package com.example.se_app;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.se_app.dto.RankDTO;
 import com.example.se_app.dto.RecordDTO;
 import com.example.se_app.service.Service;
 
@@ -15,6 +17,7 @@ import retrofit2.Response;
 
 public class TimeActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
     Button btn_day = findViewById(R.id.btn_day);
     Button btn_time = findViewById(R.id.btn_time);
 
@@ -23,10 +26,10 @@ public class TimeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time);
 
-        Call<RecordDTO.locationResponse> call = service.timerank();
-        call.enqueue(new Callback<RecordDTO.Rank>() {
+        Call<RankDTO.RankResponse> call = service.timerank;
+        call.enqueue(new Callback<RankDTO.RankResponse>() {
             @Override
-            public void onResponse(Call<RecordDTO.Rank> call, Response<RecordDTO.Rank> response) {
+            public void onResponse(Call<RankDTO.RankResponse> call, Response<RankDTO.RankResponse> response) {
                 if( response.isSuccessful()) {
                     for(int i = 0; i < 5; i++) {
                         String memberId = response.body().getMemberId();
@@ -36,17 +39,17 @@ public class TimeActivity extends AppCompatActivity {
                     }
                 }
                 else {
-                    //404 error
-                    String message = "저장된 회원 정보가 없습니다.";
-                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+                    // 응답 실패(404)
+                    String message = response.body().getMessage();
+                    Toast.makeText(getApplicationContext(), message,Toast.LENGTH_SHORT).show();
 
                 }
             }
             //서버와 통신 실패
             @Override
-            public void onFailure(Call<RecordDTO.Rank> call, Throwable t) {
-                String message = "서버와 연결이 끊겼습니다.";
-                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+            public void onFailure(Call<RankDTO.RankResponse> call, Throwable t) {
+                Toast.makeText(TimeActivity.this, "서버와 통신을 실패하였습니다.", Toast.LENGTH_SHORT).show();
+                Log.e(TAG, "서버 통신 실패: " + t.getMessage());
             }
 
         });
--- app/src/main/java/com/example/se_app/dto/RecordDTO.java
+++ app/src/main/java/com/example/se_app/dto/RecordDTO.java
@@ -1,44 +1,27 @@
 package com.example.se_app.dto;
 
 public class RecordDTO {
-    /* 랭킹보기 */
-    public static class Rank {
-        private String memberId;
-        private String memberName;
-        private String memberMajor;
-        private int recordTime;
-
-        //setter
-        public String getMemberId(String memberId) { return memberId; }
-        public String getMemberName(String memberName) { return memberName; }
-        public String getMemberMajor(String memberId) { return memberMajor; }
-        public int getRecordTime(int recordTime) { return recordTime; }
-
-        //getter
-        public void setMemberId() { this.memberId = memberId; }
-        public void setMemberName() { this.memberName = memberName; }
-        public void setMemberMajor() { this.memberMajor = memberMajor; }
-        public void setRecordTime() { this.recordTime = recordTime; }
-    }
 
     /* 당일 기록 조회 */
-    public static class record {
+    public static class TodayRecord {
         private int recordTime; //당일 기록시간
 
         //setter
-        public void setRecordTime(int recordTime) {this.recordTime = recordTime; }
+        public void setRecordTimeToday(int recordTime) {this.recordTime = recordTime; }
 
         //getter
         public int getRecordTimeToday() { return recordTime; }
 
     }
 
-    /* 위치 요청 */
-    public static class locationRequest {
+    /* 기록하기, 출석하기 */
+    public static class StartRecord {
         private Double userLatitude; //경도
         private Double userLongitude; //위도
+        private String message; //매세지
 
-        public locationRequest(Double userLatitude, Double userLongitude) {
+        //생성자
+        public StartRecord(Double userLatitude, Double userLongitude) {
             this.userLatitude = userLatitude;
             this.userLongitude = userLongitude;
         }
@@ -46,13 +29,66 @@ public class RecordDTO {
         //setter
         public void setUserLongitude(Double userLongitude) { this.userLatitude = userLongitude; }
         public void setUserLatitude(Double userLatitude) { this.userLatitude = userLatitude; }
-    }
-
-    /* 위치 응답 */
-    public static class locationResponse {
-        private String message; //매세지
 
         //getter
+        public Double getUserLongitude() { return userLongitude; }
+        public Double getUserLatitude() { return userLatitude; }
+        public String getMessage() { return message; }
+
+    }
+
+    /* 기록 중단 */
+    public static class StopRecord {
+        private int recordTime;
+        private Double userLatitude;
+        private Double userLongitude;
+        private String message;
+        public StopRecord(int recordTime, Double userLatitude, Double userLongitude) {
+            this.recordTime = recordTime;
+            this.userLatitude = userLatitude;
+            this.userLongitude = userLongitude;
+        }
+
+        //setter
+        public void setRecordTime(int recordTime) { this.recordTime = recordTime; }
+        public void setUserLatitude(Double userLatitude) { this.userLatitude = userLatitude; }
+
+        public void setUserLongitude(Double userLongitude) { this.userLongitude = userLongitude; }
+
+        //getter
+        public int getRecordTime() { return recordTime; }
+        public Double getUserLatitude() { return userLatitude; }
+        public Double getUserLongitude() { return userLongitude; }
         public String getMessage() { return message; }
     }
+
+
+    /* 위치 보내기 */
+    public static class Location {
+        private int recordTime;
+        private Double memberLatitude;
+        private Double memberLongitude;
+        private String message;
+        public Location(int recordTime, Double userLatitude, Double userLongitude) {
+            this.recordTime = recordTime;
+            this.memberLatitude = userLatitude;
+            this.memberLongitude = userLongitude;
+        }
+
+        //setter
+        public void setRecordTime(int recordTime) { this.recordTime = recordTime; }
+        public void setUserLatitude(Double userLatitude) { this.memberLatitude = userLatitude; }
+
+        public void setUserLongitude(Double userLongitude) { this.memberLongitude = userLongitude; }
+
+        //getter
+        public int getRecordTime() { return recordTime; }
+        public Double getMemberLatitude() { return memberLatitude; }
+
+        public Double getMemberLongitude() { return memberLongitude; }
+        public String getMessage() { return message; }
+
+    }
+
+
 }
